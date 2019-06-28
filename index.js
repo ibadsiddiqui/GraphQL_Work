086dd62f2c04cf6ab3d218547655d2c1d741217b
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-06-28 15:23:21 +0500

    --Commit: resolvers and typeDefs imported from new files

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 //Import ApolloServer for creating an instance of a apollo/graphql server.
 //Also import gql for creating your graphql schema.
 require('dotenv').config()
-const { ApolloServer, gql } = require('apollo-server');
-//Port where your server is listening to.
+const { ApolloServer } = require('apollo-server');
+const resolvers = require('./graphFiles/resolvers');
+const typeDefs = require('./graphFiles/typeDefs');
 
-//Type Definitions with your Query type, and schema query keyword set to it.
-const typeDefs = gql`
-    # Query object type.
-    type Query {
-        helloString: String,
-        index: Int
-    }
-    # Assign your query type to your Query type.
-    schema {
-        query: Query
-    }
-`
-//Define your resolvers that will define how your fields are executed.
-const resolvers = {
-    Query: {
-        helloString: () => "Hello World!",
-        index: () => 1
-
-    }
-}
 //Define your server by assigning it to your ApolloServer instance.
 const server = new ApolloServer({ resolvers, typeDefs })
 
